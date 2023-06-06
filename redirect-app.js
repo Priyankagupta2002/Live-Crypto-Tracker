b5478e9289ad0db03c6d7489ed7dd18f798fb1a6
CODER-P <118664960+Priyankagupta2002@users.noreply.github.com>
2023-06-06 14:08:58 +0530

    Update redirect-app.js

--- redirect-app.js
+++ redirect-app.js
@@ -1,4 +1,4 @@
 document.getElementById("cryptoBtn").onclick = function(){
     var cryppNme = document.getElementById("cryptoName").value;
-    window.location.href = "https://c0der-p.github.io/Live-Crypto-Tracker/crypto.html?crypto="+cryppNme; // add your website link instead of mines here :)
+    window.location.href = "https://priyankagupta2002.github.io/Live-Crypto-Tracker/crypto.html?crypto="+cryppNme; // add your website link instead of mines here :)
 }
